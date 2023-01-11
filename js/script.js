6ee5be59c76894e05c9f9823596013cc478c1745
Jonathon Newell <jonathonnewell123@gmail.com>
2023-01-11 13:18:00 -0500

    Added user info functions and fetched github api

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const overview = document.querySelector(".overview");
+// const overview is selecting the div w/ the class of "overview". This div is where profile information will appear
+const username = TieFit;
+
+const gitUserInfo = async function () {
+    const userInfo = await fetch(`https://api.github.com/users/${username}`);
+    const data = await userInfo.json();
+    displayUserInfo(data);
+};
+
+gitUserInfo();
+
+const displayUserInfo = function (data) {
+    const div = document.createElement("div");
+    div.classList.add("user-info");
+    div.innerHTML = `
+        <figure>
+            <img alt="user avatar" src=${data.avatar_url} />
+        </figure>
+        <div>
+            <p><strong>Name:</strong> ${data.name}</p>
+            <p><strong>Bio:</strong> ${data.bio}</p>
+            <p><strong>Location:</strong> ${data.location}</p>
+            <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
+        </div>
+    `;
+    overview.append(div);
+};
